c368aee61c113a596668c83fd1844265a12891c5
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-17 17:00:01 -0500

    index.html structure, first steps index.js

--- webpack-demo/src/index.js
+++ webpack-demo/src/index.js
@@ -1,11 +1,26 @@
 import _ from 'lodash';
 import './style.css';
 
-function component() {
-  const listContainer = document.querySelector('.list-container');
+const listArray = [
+  {
+    description: '',
+    completed: false,
+    index: 1,
+  }
+]
+
+function add() {
+  const listContainer = document.querySelector('.list-items');
   const listElement = document.createElement('li');
-  listElement.innerHTML = 'hello';
+  listElement.classList.add('list-element');
   listContainer.appendChild(listElement);
+  const checkButton = document.createElement('button');
+  const description = document.createElement('h3');
+  const moveButton = document.createElement('svg');
+  listElement.appendChild(checkButton);
+  listElement.appendChild(description);
+  listElement.appendChild(moveButton);
+
 }
 
-component();
+add();
