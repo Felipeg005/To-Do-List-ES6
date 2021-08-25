b7ac6aee0720dcb4eb12b11f7f4e5c3d8590817b
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-25 13:05:13 -0500

    install jest, add initial files

---
+++ src/js/test/add.test.js
@@ -0,0 +1,20 @@
+describe()
+
+const add = () => {
+  let listArray = JSON.parse(localStorage.getItem('taskStorage'));
+  if (!listArray) {
+    listArray = [];
+  }
+  const newInput = document.getElementById('new-item');
+  const newTask = {
+    idContainer: `list-element move-button${listArray.length + 1}`,
+    idInput: `input-text${listArray.length + 1}`,
+    idCheckbox: `input-checkbox${listArray.length + 1}`,
+    description: `${newInput.value}`,
+    completed: false,
+    index: listArray.length + 1,
+  };
+  listArray.push(newTask);
+  localStorage.setItem('taskStorage', JSON.stringify(listArray));
+};
+export default add;
