9e54d954a308d35d042b25d10c66b8f5a4260238
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-26 11:35:42 -0500

    add test for li elements add function

--- src/js/__test__/add.test.js
+++ src/js/__test__/add.test.js
@@ -4,13 +4,33 @@ jest.mock('../modules/add');
 describe('test add function', () => {
 
   describe('add', () => {
+
     test('must add localStorage element', () => {
       // Arrange
+      document.body.innerHTML = '<ul class="list-items"></ul>';
+      let listArray = JSON.parse(localStorage.getItem('taskStorage'));
+      if (!listArray) {
+        listArray = [];
+      }
+      const newInput = document.querySelector('.list-items');
+      const newTask = {
+        idContainer: `list-element move-button${listArray.length + 1}`,
+        idInput: `input-text${listArray.length + 1}`,
+        idCheckbox: `input-checkbox${listArray.length + 1}`,
+        description: `${newInput}`,
+        completed: false,
+        index: listArray.length + 1,
+      };
+      listArray.push(newTask);
+      localStorage.setItem('taskStorage', JSON.stringify(listArray));
+      newInput.innerHTML += '<li>listElement</li>';
+      let result = document.querySelectorAll('li');
     
       // Act
 
       // Assert
-      expect(add()).toBe(1);
+      expect(listArray.length).toBe(1);
+      expect(result.length).toBe(1);
     })
   ;})
 
--- src/js/modules/__mocks__/add.js
+++ src/js/modules/__mocks__/add.js
@@ -1,19 +1,2 @@
-export const add = () => {
-  let listArray = JSON.parse(localStorage.getItem('taskStorage'));
-  if (!listArray) {
-    listArray = [];
-  }
-  const newInput = document.getElementById('new-item');
-  const newTask = {
-    idContainer: `list-element move-button${listArray.length + 1}`,
-    idInput: `input-text${listArray.length + 1}`,
-    idCheckbox: `input-checkbox${listArray.length + 1}`,
-    description: `${newInput}`,
-    completed: false,
-    index: listArray.length + 1,
-  };
-  listArray.push(newTask);
-  localStorage.setItem('taskStorage', JSON.stringify(listArray));
-  return listArray.length;
-};
+export const add = () => {};
 
