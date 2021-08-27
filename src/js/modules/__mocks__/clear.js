057c84b84d114501930b162c160f1e0aa665a4ad
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-26 19:55:39 -0500

    Add clear and description edit functions tests

--- src/js/modules/__mocks__/clear.js
+++ src/js/modules/__mocks__/clear.js
@@ -1,9 +1,2 @@
-const clear = () => {
-  const listArray = JSON.parse(localStorage.getItem('taskStorage'));
-  /* eslint-disable-line */const trueValues = listArray.filter((e) => {
-    return e.completed === false;
-  });
-  localStorage.setItem('taskStorage', JSON.stringify(trueValues));
-  /* eslint-disable-line */location.reload();
-};
+const clear = () => {};
 export default clear;
