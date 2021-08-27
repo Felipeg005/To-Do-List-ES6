833f430cb0fc5c282da9d3b11568c8f3c2c184bc
Felipeg005 <juanfelipegomeztobon@gmail.com>
2021-08-26 19:08:09 -0500

    add tests files clear functions

---
+++ src/js/__test__/clear.test.js
@@ -0,0 +1,19 @@
+import clear from '../modules/clear';
+
+jest.mock('../modules/clear');
+
+describe('test clear function', () => {
+  describe('clear', () => {
+    test('must clear localStorage and li elements', () => {
+      // Arrange
+      
+
+      // Act
+
+
+      // Assert
+
+    });
+  });
+});
+export default clear;
